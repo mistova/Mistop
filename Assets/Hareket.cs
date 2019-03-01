482124cded018153997411d35317675a19c395dc
mistova <33962976+mistova@users.noreply.github.com>
2019-03-01 12:50:01 +0300

    Game
    Only a game

---
+++ Assets/Hareket.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hareket : MonoBehaviour
+{
+    public Rigidbody force;
+    public Vector3 a;
+    // Use this for initialization
+    void Start()
+    {
+        force = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        force.AddForce(Input.GetAxis("Horizontal") * 30, 0, Input.GetAxis("Vertical") * 30);
+    }
+    void LateUpdate()
+    {
+        if (transform.position.y < 0)
+        {
+            a.x = 0;
+            a.y = 1;
+            a.z = 0;
+            transform.position = a;
+        }
+    }
+}
---
+++ Assets/Kamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kamera : MonoBehaviour
+{
+    private Vector3 uzaklik;
+    public GameObject takip;
+
+    // Use this for initialization
+    void Start()
+    {
+        uzaklik = transform.position - takip.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void LateUpdate()
+    {
+        transform.position = uzaklik + takip.transform.position;
+    }
+}
